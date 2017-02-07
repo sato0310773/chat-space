e44c062e95a13718c0b9847c2927cdf8adaaedbd
satotaku <sato0310773@gmail.com>
2017-02-08 02:43:02 +0900

    migration user

--- db/migrate/20170207173250_add_devise_to_users.rb
+++ db/migrate/20170207173250_add_devise_to_users.rb
@@ -1,6 +1,6 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
+class AddDeviseToUsers < ActiveRecord::Migration[5.0]
+  def self.up
+    change_table :users do |t|
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string   :reset_password_token
@@ -11,10 +11,13 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
-      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
   end
 end

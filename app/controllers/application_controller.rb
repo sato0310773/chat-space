c9e240352263f6ed6b1cef6eb77d0db7822e0520
satotaku <sato0310773@gmail.com>
2017-04-18 12:33:56 +0900

    root_path after sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def after_sign_out_path_for(resource)
+  	'/users/sign_in'
+  end
+
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   	devise_parameter_sanitizer.permit(:account_update, keys: [:name])

c28c6989ebb592b0dfa8e4430a754102099f785c
satotaku <sato0310773@gmail.com>
2017-04-18 12:32:48 +0900

    root devise/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root  'messages#index'
   resources :messages, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

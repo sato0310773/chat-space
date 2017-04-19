955cf63d54bef5d3238634cfafab23e3b3465327
satotaku <sato0310773@gmail.com>
2017-04-19 14:05:42 +0900

    delete after_sign_out
    この実装では
    まだ必要なかったので、一度削除します

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
-  def after_sign_out_path_for(resource)
-  	'/users/sign_in'
-  end
+
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])

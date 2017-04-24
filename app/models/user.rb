523dba62f019538cce22596e01cde5f6caa8b59d
satotaku <sato0310773@gmail.com>
2017-04-24 15:42:02 +0900

    create controller & model 'group'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+ # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true
-
-  has_many: :messages
 end
